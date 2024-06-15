2f889a71f81abe737c496d5a5d38781293b9d31c
Joerg Benke <j.benke@fz-juelich.de>
2024-06-15 22:49:07 +0200

    Code adapted to PEP8

--- climate_change.py
+++ climate_change.py
@@ -3,12 +3,14 @@ This is a small project to get familiar with the very basic ideas
 of climate change modelling and Github Actions
 """
 
-def energy_balance( albedo=1, emissivity=1, swr=1 ):
+
+def energy_balance(albedo=1, emissivity=1, swr=1):
     """
     Implementation of the stationary balance of incoming
     and outgoing radiation
     """
     return albedo * emissivity * swr
 
+
 if __name__ == "__main__":
-    print( energy_balance( 1, 1, 1 ) )
+    print(energy_balance(1, 1, 1))
