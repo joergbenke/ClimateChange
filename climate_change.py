ba27dc8ed88a308a82c67545d28125f867a5418b
Joerg Benke <j.benke@fz-juelich.de>
2024-06-15 20:34:09 +0200

    Module docstrings added

--- climate_change.py
+++ climate_change.py
@@ -11,5 +11,5 @@ def energy_balance( albedo=1, emissivity=1, swr=1 ):
     return albedo * emissivity * swr
 
 if __name__ == "__main__":
-    temperature = energyBalance( 1, 1, 1 )
+    temperature = energy_balance( 1, 1, 1 )
     print( temperature)
