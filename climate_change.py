bf51276a50c342b1aab5ae3c6720a2394f8f693a
Joerg Benke <j.benke@fz-juelich.de>
2024-09-25 12:32:34 +0200

    Corrections on the basis of pylint

--- climate_change.py
+++ climate_change.py
@@ -15,10 +15,16 @@ def energy_balance(albedo=1, emissivity=1, swr=2):
 # Testregion
 
 def test_energy_balance():
-    assert(energy_balance(1, 1, 2) == 2)
+    """
+    Test with 
+    """
+    assert energy_balance(1, 1, 2) == 2
 
 def test_energy_balance_zeroes():
-    assert(energy_balance(0, 0, 0) == 2)
+    """
+    Test with 
+    """
+    assert energy_balance(0, 0, 0) == 2
 
 
 if __name__ == "__main__":
