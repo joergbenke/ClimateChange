dfdd0076cdd718d3f8f087cbd6bdd4eb07c743ba
Joerg Benke <j.benke@fz-juelich.de>
2024-09-25 12:14:46 +0200

    Added some tests to climate_change.py

--- climate_change.py
+++ climate_change.py
@@ -11,6 +11,17 @@ def energy_balance(albedo=1, emissivity=1, swr=2):
     """
     return albedo * emissivity * swr
 
+#
+# Testregion
+
+def test_energy_balance():
+    assert(energy_balance(1, 1, 2) == 2)
+
+def test_energy_balance_zeroes():
+    assert(energy_balance(0, 0, 0) == 2)
+
 
 if __name__ == "__main__":
+    test_energy_blance()
+    test_energy_blance_zeroes()
     print(energy_balance(1, 1, 1))
