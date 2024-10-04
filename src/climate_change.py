7b3b47754ca48d8413d314b2a5d78bd84d2d1ab1
Joerg Benke <j.benke@fz-juelich.de>
2024-10-05 00:19:36 +0200

    Added some test according to chapter 2 of the tutorial

--- src/climate_change.py
+++ src/climate_change.py
@@ -6,25 +6,25 @@ We implement a small 0 dimensional energy balance model
 """
 
 
-def absorbed_shortwave_radiation(albedo=0.299):
+def absorbed_shortwave_radiation(albedo=0.299, iswr=341.3):
     """
     Implementation of the stationary balance of incoming
     and outgoing radiation
+
+    iswr = global mean insulation
     """
-    iswr = 341.3  # global mean insulation
 
     return (1 - albedo) * iswr
 
 
-def outgoing_longwave_radiation(temperature):
+def outgoing_longwave_radiation(tau, temperature):
     """
     Calculation of the outgoing radiation of the earth
     (earth is assumed to be an approximated black body radiator
     and the transmissivitiy of the atmosphere is also taken into
     account)
     """
-    theta = 5.67 * 10 ** (-8)
-    tau = 0.61  # transmissivity of the atmosphere
+    theta = 5.67 * 10 ** (-8)  # Stefan-Boltzmann-Konstante
 
     return theta * tau * temperature ** 4
 
@@ -34,7 +34,7 @@ def black_body_radiation(temperature):
     Calculation of the black body radiation of the earth
     (earth is assumed to be an approximated black body radiator)
     """
-    theta = 5.67 * 10 ** (-8)
+    theta = 5.67 * 10 ** (-8)  # Stefan-Boltzmann-Konstante
 
     return theta * temperature ** 4
 
@@ -62,8 +62,12 @@ def temperature_equilibrium_earth(tau, albedo, iswr):
 
 if __name__ == "__main__":
     # Short wave radiation implementation
-    print(absorbed_shortwave_radiation(0.299))
+    print("Absorbed shortwave energy")
+    print(absorbed_shortwave_radiation(0.32, 341.3))
+    print("Outgoing longwave radiation")
+    print(outgoing_longwave_radiation(0.57, 291))
 
+    print()
     print(black_body_radiation(288))
     print(black_body_radiation(292))
     # Output of the temperature of the black body radiator earth with
@@ -72,3 +76,18 @@ if __name__ == "__main__":
 
     #
     # Some tests with the equilibrium tmeperature
+    print("Temperature (surface) at equilibrium")
+    print("Arguments: tau=0.61, albedo=0.299, iswr=341.3")
+    print(temperature_equilibrium_earth(0.61, 0.299, 341.3))
+    print()
+    print("Arguments: tau=0.57, albedo=0.299, iswr=341.3")
+    print(temperature_equilibrium_earth(0.57, 0.299, 341.3))
+    print()
+    print("Arguments: tau=0.61, albedo=0.32, iswr=341.3")
+    print(temperature_equilibrium_earth(0.61, 0.32, 341.3))
+    print()
+    print("Arguments: tau=0.57, albedo=0.32, iswr=341.3")
+    print(temperature_equilibrium_earth(0.57, 0.32, 341.3))
+    print()
+    print("Arguments: tau=1.0, albedo=0.299, iswr=341.3")
+    print(temperature_equilibrium_earth(1.0, 0.299, 341.3))
