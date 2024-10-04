fb150a0655c05d91a619372bfe24694d83cef8cd
Joerg Benke <j.benke@fz-juelich.de>
2024-10-04 23:01:26 +0200

    Added the new function temperature_equilibrium_earth
    Corrected some comments

--- src/climate_change.py
+++ src/climate_change.py
@@ -3,17 +3,9 @@ import math as m
 """
 This is a small project to get familiar with the very basic ideas
 of climate change modelling and Github Actions
+
+We implement a small 0 dimensional energy balance model
 """
-
-
-def energy_balance(albedo=1, emissivity=1, swr=2):
-    """
-    Implementation of the stationary balance of incoming
-    and outgoing radiation
-    """
-
-    return albedo * emissivity * swr
-
 
 def absorbed_shortwave_radiation(albedo=0.299):
     """
@@ -25,15 +17,27 @@ def absorbed_shortwave_radiation(albedo=0.299):
     return (1 - albedo) * Q
 
 
+def outgoing_longwave_radiation(temperature):
+    """
+    Calculation of the outgoing radiation of the earth
+    (earth is assumed to be an approximated black body radiator
+    and the transmissivitiy of the atmosphere is also taken into
+    account)
+    """
+    STEFAN_BOLTZMANN_CONSTANT = 5.67 * 10 ** (-8)
+    TAU = 0.61  # transmissivity of the atmosphere 
+    
+    return STEFAN_BOLTZMANN_CONSTANT * TAU * temperature ** 4
+
+
 def black_body_radiation(temperature):
     """
     Calculation of the black body radiation of the earth
     (earth is assumed to be an approximated black body radiator)
     """
     STEFAN_BOLTZMANN_CONSTANT = 5.67 * 10 ** (-8)
-    TAU = 0.61  # transmissivity of the atmosphere 
     
-    return STEFAN_BOLTZMANN_CONSTANT * TAU * temperature ** 4
+    return STEFAN_BOLTZMANN_CONSTANT * temperature ** 4
 
 
 def temperature_black_body_radiator(olr):
@@ -46,9 +50,18 @@ def temperature_black_body_radiator(olr):
     return (olr / STEFAN_BOLTZMANN_CONSTANT) ** (1/4)
 
 
-if __name__ == "__main__":
-    print(energy_balance(1, 1, 1))
+def temperature_equilibrium_earth(tau, albedo, Q):
+    """
+    Calculate the equilibrium temprature of the earth with the help
+    of the arguments albedo, transmissivity of the atmosphere and 
+    the incoming short wave radiation
+    """
+    STEFAN_BOLTZMANN_CONSTANT = 5.67 * 10 ** (-8)
+    
+    return ((1 - albedo) * Q / (tau * STEFAN_BOLTZMANN_CONSTANT)) ** (1/4)
 
+
+if __name__ == "__main__":
     # Short wave radiation implementation
     print(absorbed_shortwave_radiation(0.299))
     
@@ -57,3 +70,6 @@ if __name__ == "__main__":
     # Output of the temperature of the black body radiator earth with
     # given outgoing longwave radiation
     print(temperature_black_body_radiator(238.5))
+
+    #
+    # Some tests with the equilibrium tmeperature
